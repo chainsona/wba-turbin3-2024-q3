f2ad39d136f05db4823f60c88f92178d6cade0bc
Soona <code@chainsona.dev>
2024-07-31 02:30:20 +0200

    feat: add week 1 part 1/3 delivrables

--- docs/course/week1/README.md
+++ docs/course/week1/README.md
@@ -1,1 +1,23 @@
 # Turbin3 Course Week 1
+
+## 20240730 - Part 1/3
+
+## Initialize a token
+
+```sh
+cd ts/cluster1
+
+npx ts-node spl_init.ts
+```
+
+[Initialization transaction](https://explorer.solana.com/address/xGH6b6B8gBqrPse7LiFtiVeQWWVx5NSVQ6berXpXzpT?cluster=devnet)
+
+## Mint to an account
+
+```sh
+cd ts/cluster1
+
+npx ts-node spl_mint.ts
+```
+
+[Mint transaction](https://explorer.solana.com/tx/2SEchd4nFrQqzBTdLh18TL8fCBofUEhN9kbTaCy9LHwdaS59a2Y3TYE76hUew6NFamNV9MF9QFMkDPjcHB8v9jjd?cluster=devnet)
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,6 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -9,21 +9,40 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-const token_decimals = 1_000_000n;
+const token_decimals = 9;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("xGH6b6B8gBqrPse7LiFtiVeQWWVx5NSVQ6berXpXzpT");
 
 (async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+  try {
+    // Create an ATA
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log(
+      `Your ata is: ${ata.address.toBase58()} https://explorer.solana.com/tx/${
+        ata.address
+      }?cluster=devnet`
+    );
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+    // Mint to ATA
+    const amount = 10 ** token_decimals;
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      amount
+    );
+    console.log(
+      `Your mint txid: https://explorer.solana.com/tx/${mintTx}?cluster=devnet`
+    );
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
